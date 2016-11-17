d070cbfd105fbffb44a101246216c1080d7ed678
Eduardo Gonzalez <eduardo.gch@gmail.com>
2016-11-16 20:33:11 -0500

    Add chimp.js for bdd testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
+  connect = require('gulp-connect'),
+  chimp = require('gulp-chimp');
 
-gulp.task('connect', function() {
+gulp.task('connect', function () {
   connect.server({
     root: 'app',
     livereload: true
@@ -15,6 +16,30 @@ gulp.task('html', function () {
 
 gulp.task('watch', function () {
   gulp.watch(['./app/*.html'], ['html']);
+});
+
+gulp.task('bdd', ['connect'], function () {
+    return chimp({
+        features: './test/bdd', // Cucumber features files
+        browser: 'phantomjs',
+        singleRun: true,
+        debug: false,
+        output: {
+            screenshotsPath: './e2e_output/screenshots',
+            jsonOutput: './e2e_output/cucumber.json'
+        },
+        htmlReport: {
+            enable: true,
+            jsonFile: './e2e_output/cucumber.json',
+            output: './e2e_output/report/cucumber.html',
+            reportSuiteAsScenarios: true,
+            launchReport: true
+        }
+    });
+});
+
+gulp.task('tdd', function () {
+    console.log('hola');
 });
 
 gulp.task('default', ['connect', 'watch']);
